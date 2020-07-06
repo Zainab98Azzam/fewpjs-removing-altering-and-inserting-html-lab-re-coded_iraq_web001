46b3e6a04b1ce244953fd9872103f7e6c10b2c06
Zainab Azzam <zainab_azzam@yahoo.com>
2020-07-06 13:00:32 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,6 +0,0 @@
-let element = document.querySelector("body");
-element.removeChild(document.querySelector("main#main"))
-
-let newHeader=document.querySelector("h1#victory")
-
-newHeader.innerHTML='is the champion';
