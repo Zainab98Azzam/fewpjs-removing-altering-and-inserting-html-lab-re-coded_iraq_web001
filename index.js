1c0c709c8f450be4dd4cf4994ec6d8cfbb716686
Zainab Azzam <zainab_azzam@yahoo.com>
2020-07-06 13:00:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,6 @@
+let element = document.querySelector("body");
+element.removeChild(document.querySelector("main#main"))
+
+let newHeader=document.querySelector("h1#victory")
+
+newHeader.innerHTML='is the champion';
